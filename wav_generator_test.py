3dee35e27f21c948f589e7640fd5f62b7f950adf
cmdenis <christianmdenis@gmail.com>
2022-11-15 22:53:53 -0500

    Working event class

--- wav_generator_test.py
+++ wav_generator_test.py
@@ -3,10 +3,69 @@ from scipy.io.wavfile import write
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-samplerate = 44100; fs = 100
-t = np.linspace(0., 1., samplerate)
-amplitude = np.iinfo(np.int16).max
-data = amplitude * np.sin(2. * np.pi * fs * t)
+
+# Sampling rate
+samplerate = 44100
+length = 7 # (Seconds)
+
+
+class Event:
+    def __init__(self, pitch, start, end):
+        # Handling possible misuse cases
+        if start > end:
+            raise NameError("Starting Time is After End Time...")
+        if start < 0:
+            raise NameError("Starting Time Before 0...")
+        if end > length:
+            raise NameError("Ending time greater than duration of the song...")
+
+        
+        # Initializing attributes
+        self.pitch = pitch              # Pitch of sound
+        self.start = start              # Time of start of sound
+        self.end = end                  # Time of end of sound
+        #self.modulation = modulation    # Array with modulation data, WIP
+
+    def sine(self):
+        # Creating a sine wave
+
+        samples = (self.end - self.start) * samplerate
+
+        t0 = np.zeros(int(self.start*samplerate))                       # Zeroes before start of sound
+        t1 = np.sin(2*np.pi*self.pitch*np.arange(samples)/samplerate)   # Sound
+        t2 = np.zeros(int((length - self.end)*samplerate))              # Zeroes at the end of sound
+        buffer = np.zeros(10)   # Buffer to make all arrays of equal length
+
+        return np.concatenate((t0, t1, t2, buffer))[:samplerate*length]
+
+    
+
+beep1 = Event(200, 0.5, 3)
+beep2 = Event(1000, 0.6, 3)
+beep3 = Event(2000, 0.7, 3)
+beep4 = Event(500, 4, 6)
+beep5 = Event(700, 4.5, 6)
+
+# Making a list of events.
+# Each events corresponds to the start of a musical note (or sound)
+# There 
+event_list = np.array([
+    beep1.sine(),
+    beep2.sine(),
+    beep3.sine(),
+    beep4.sine(),
+    beep5.sine()
+])
+
+
+
+signal = np.sum(event_list, axis = 0) 
+
+signal = signal / np.max(signal) # Normalizing
+
+
+# Making sure signal has correct amplitude
+data = np.iinfo(np.int16).max * signal/1.1
 write("example.wav", samplerate, data.astype(np.int16))
 
 
@@ -16,5 +75,7 @@ samplerate, data = wavfile.read("example.wav")
 times = np.arange(len(data))/samplerate
 
 
-plt.plot(times, data)
+plt.plot(times, data/np.max(data))
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
 plt.show()
